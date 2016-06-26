ed25d086764360067e1cffe2b95934ef4a03011d
Josh Wyant <joshwyant91@gmail.com>
2016-06-26 01:47:40 -0500

    Moving stuff around

--- GameCreator.Contracts.Services/GmSettings.cs
+++ GameCreator.Contracts.Services/GmSettings.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameCreator.Projects
+namespace GameCreator.Contracts.Services
 {
     public class GmSettings : IGameMakerSettings
     {
