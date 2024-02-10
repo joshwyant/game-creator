9c8807bb4ca4b3e7318647498c4fa5a200e28cd2
Josh <1755797+joshwyant@users.noreply.github.com>
2024-02-09 20:37:43 -0800

    Revert "Runs fine for now without action libraries"
    This reverts commit 750baffaddb6a56c97c9c8befc36f9ce512b47c9.

---
+++ GameCreator.ActionLibraries.BinaryFiles/ActionLibraryReader.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using GameCreator.Resources.Api;
+
+namespace GameCreator.ActionLibraries.BinaryFiles
+{
+    public class ActionLibraryReader : IDisposable
+    {
+        public Stream InputStream { get; }
+
+        public ActionLibraryReader(string filename)
+            : this(new FileStream(filename, FileMode.Open))
+        {
+            
+        }
+        
+        public ActionLibraryReader(Stream inputStream)
+        {
+            InputStream = inputStream;
+        }
+
+        private bool _alreadyRead;
+        public ActionLibrary Read()
+        {
+            if (_alreadyRead)
+            {
+                throw new EndOfStreamException();
+            }
+
+            _alreadyRead = true;
+
+            using (var br = new BinaryReader(InputStream, Encoding.ASCII))
+            {
+                byte[] ReadPrefixedBytes()
+                {
+                    var bytes = new byte[br.ReadInt32()];
+                    br.Read(bytes, 0, bytes.Length);
+                    return bytes;
+                }
+
+                int ver;
+                return new ActionLibrary
+                {
+                    GameMakerVersion = br.ReadInt32(),
+                    TabCaption = new string(br.ReadChars(br.ReadInt32())),
+                    LibraryID = br.ReadInt32(),
+                    Author = new string(br.ReadChars(br.ReadInt32())),
+                    Version = br.ReadInt32(),
+                    LastChanged = new DateTime(1899, 12, 30).AddDays(br.ReadDouble()),
+                    Info = new string(br.ReadChars(br.ReadInt32())),
+                    InitializationCode = new string(br.ReadChars(br.ReadInt32())),
+                    AdvancedModeOnly = br.ReadInt32() != 0,
+                    LastActionId = br.ReadInt32(),
+                    Actions = Enumerable
+                        .Range(0, br.ReadInt32())
+                        .Select(i => new ActionDefinition
+                        {
+                            GameMakerVersion = ver = br.ReadInt32(),
+                            Name = new string(br.ReadChars(br.ReadInt32())),
+                            ActionID = br.ReadInt32(),
+                            Image = ReadPrefixedBytes(),
+                            Hidden = br.ReadInt32() != 0,
+                            Advanced = br.ReadInt32() != 0,
+                            RegisteredOnly = ver != 500 && br.ReadInt32() != 0,
+                            Description = new string(br.ReadChars(br.ReadInt32())),
+                            ListText = new string(br.ReadChars(br.ReadInt32())),
+                            HintText = new string(br.ReadChars(br.ReadInt32())),
+                            Kind = (ActionKind) br.ReadInt32(),
+                            InterfaceKind = (ActionInterfaceKind) br.ReadInt32(),
+                            IsQuestion = br.ReadInt32() != 0,
+                            ShowApplyTo = br.ReadInt32() != 0,
+                            ShowRelative = br.ReadInt32() != 0,
+                            ArgumentCount = br.ReadInt32(),
+                            Arguments = Enumerable.Range(0, br.ReadInt32())
+                                .Select(j => new ActionArgument
+                                {
+                                    Caption = new string(br.ReadChars(br.ReadInt32())),
+                                    Type = (ActionArgumentType) br.ReadInt32(),
+                                    DefaultValue = new string(br.ReadChars(br.ReadInt32())),
+                                    Menu = new string(br.ReadChars(br.ReadInt32()))
+                                }).ToArray(),
+                            ExecutionType = (ActionExecutionType) br.ReadInt32(),
+                            FunctionName = new string(br.ReadChars(br.ReadInt32())),
+                            Code = new string(br.ReadChars(br.ReadInt32()))
+                        }).ToList()
+                };
+            }
+        }
+
+        public void Dispose()
+        {
+            InputStream?.Dispose();
+        }
+    }
+}
---
+++ GameCreator.ActionLibraries/ActionDefinition.cs
@@ -0,0 +1,67 @@
+﻿using GameCreator.Resources.Api;
+
+namespace GameCreator.ActionLibraries
+{
+    public class ActionDefinition
+    {
+        public int GameMakerVersion { get; set; }
+        public string Name { get; set; }
+        public int ActionID { get; set; }
+        public byte[] Image { get; set; }
+        public bool Hidden { get; set; }
+        public bool Advanced { get; set; }
+        public bool RegisteredOnly { get; set; }
+        public string Description { get; set; }
+        public string ListText { get; set; }
+        public string HintText { get; set; }
+        public ActionKind Kind { get; set; }
+        public ActionInterfaceKind InterfaceKind { get; set; }
+        public bool IsQuestion { get; set; }
+        public bool ShowApplyTo { get; set; }
+        public bool ShowRelative { get; set; }
+        public int ArgumentCount { get; set; }
+        public ActionArgument[] Arguments { get; set; }
+        public ActionExecutionType ExecutionType { get; set; }
+        public string FunctionName { get; set; }
+        public string Code { get; set; }
+
+        public ActionDefinition()
+        {
+            
+        }
+        
+        public ActionDefinition(string name, int id, byte[] image)
+        {
+            GameMakerVersion = 520;
+            Name = name;
+            ActionID = id;
+            Image = image;
+            Hidden = false;
+            Advanced = false;
+            RegisteredOnly = false;
+            Description = string.Empty;
+            ListText = string.Empty;
+            HintText = string.Empty;
+            Kind = ActionKind.Normal;
+            InterfaceKind = ActionInterfaceKind.Normal;
+            IsQuestion = false;
+            ShowApplyTo = true;
+            ShowRelative = true;
+            ArgumentCount = 0;
+            Arguments = new ActionArgument[8];
+            for (var i = 0; i < 8; i++)
+            {
+                Arguments[i] = new ActionArgument();
+            }
+            ExecutionType = ActionExecutionType.None;
+            FunctionName = string.Empty;
+            Code = string.Empty;
+        }
+
+        public ActionDefinition(ActionLibrary library)
+            : this($"Action {++library.LastActionId}", library.LastActionId, null)
+        {
+            
+        }
+    }
+}
