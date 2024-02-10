3272087e7d97429230eb1d6914df9355c1b009e2
Josh <1755797+joshwyant@users.noreply.github.com>
2024-02-09 16:13:11 -0800

    Clean up ImageLoader.cs

--- TestGame/ImageLoader.cs
+++ TestGame/ImageLoader.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using GameCreator.Resources.Api;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace TestGame
@@ -36,26 +35,15 @@ namespace TestGame
 
         public unsafe IImage FromStream(Stream s)
         {
-            using (var image = SixLabors.ImageSharp.Image.Load(s))
+            using (var image = Image.Load(s))
             {
                 var pixels = new Rgba32[image.Width * image.Height];
                 (image.Frames[0] as ImageFrame<Rgba32>).CopyPixelDataTo(pixels);
 
-                // //fixed (uint* data = &image.Frames[0].DangerousGetPinnableReferenceToPixelBuffer())
-                // {
-                //     //&image.Frames[0].CopyPixelsTo(data);
-                //     for (var i = 0; i < pixels.Length; i++)
-                //     {
-                //         var pixel = pixelSpan[i];
-                //         // Pack the RGBA channels into a single uint
-                //         pixels[i] = (uint)((pixel.R << 24) | (pixel.G << 16) | (pixel.B << 8) | pixel.A);
-                //     }
-                // }
-
-                var bytes = pixels
+                var colors = pixels
                     .Select(pixel => (uint)(((pixel.A) << 24) | ((pixel.B) << 16) | ((pixel.G) << 8) | pixel.R))
                     .ToArray();
-                return new LoadedImage(image.Width, image.Height, bytes);
+                return new LoadedImage(image.Width, image.Height, colors);
             }
         }
     }
