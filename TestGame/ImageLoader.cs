e171b0782682dbb19d5d2bc05de51cd40bb5fa44
Josh <1755797+joshwyant@users.noreply.github.com>
2024-02-10 18:58:59 -0800

    Revert "Remove TestGame."
    This reverts commit 8e2fb9243c6a1d6eeab6b67c5abe83fc1cc8d7ee.

---
+++ TestGame/ImageLoader.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+using GameCreator.Api.Resources;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace TestGame
+{
+    public class ImageLoader
+    {
+        private class LoadedImage : IImage
+        {
+            public int Width { get; }
+            public int Height { get; }
+            public uint[] ImageData { get; }
+
+            public LoadedImage(int width, int height, uint[] imageData)
+            {
+                Width = width;
+                Height = height;
+                ImageData = imageData;
+            }
+        }
+
+        public IImage[] LoadImages(params string[] fileNames)
+        {
+            return fileNames.Select(fileName =>
+            {
+                using (var fs = File.Open(fileName, FileMode.Open))
+                {
+                    return FromStream(fs);
+                }
+            }).ToArray();
+        }
+
+        public unsafe IImage FromStream(Stream s)
+        {
+            using (var image = Image.Load(s))
+            {
+                var pixels = new Rgba32[image.Width * image.Height];
+                (image.Frames[0] as ImageFrame<Rgba32>).CopyPixelDataTo(pixels);
+
+                var colors = pixels
+                    .Select(pixel => (uint)(((pixel.A) << 24) | ((pixel.B) << 16) | ((pixel.G) << 8) | pixel.R))
+                    .ToArray();
+                return new LoadedImage(image.Width, image.Height, colors);
+            }
+        }
+    }
+}
---
+++ TestGame/TestGame.cs
@@ -0,0 +1,17 @@
+﻿using GameCreator.Engine;
+using GameCreator.Api.Engine;
+
+namespace TestGame
+{
+    public class TestGame : GameContext
+    {
+        public TestGame(IGraphicsPlugin graphics, IInputPlugin input, IAudioPlugin audio, 
+            ITimerPlugin timer, IResourceContext resourceContext)
+            : base(graphics, input, audio, timer, resourceContext)
+        {
+            timer.TargetFps = 30;
+        }
+
+        public override int GameId => 54321;
+    }
+}
